b4c4d43687d354db967d3a5a378ec9561d3c4bf9
Vlad <vladyslav.dzemiuk@oa.edu.ua>
2024-12-01 17:25:38 +0200

    Initial tests project

---
+++ Tests.Common/BaseIntegrationTest.cs
@@ -0,0 +1,69 @@
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Tests.Common;
+
+public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebFactory>
+{
+    protected readonly ApplicationDbContext Context;
+    protected readonly HttpClient Client;
+
+    protected BaseIntegrationTest(IntegrationTestWebFactory factory)
+    {
+        var scope = factory.Services.CreateScope();
+
+        Context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        Client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddAuthentication(defaultScheme: "TestScheme")
+                        .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                            "TestScheme", _ => { });
+                });
+            })
+            .CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false,
+            });
+
+        Client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue(scheme: "TestScheme");
+    }
+
+    protected async Task<int> SaveChangesAsync()
+    {
+        var result = await Context.SaveChangesAsync();
+        Context.ChangeTracker.Clear();
+
+        return result;
+    }
+}
+
+public class TestAuthHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+{
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[] { new Claim(ClaimTypes.Role, "admin"), new Claim("userId", "admin") };
+        var identity = new ClaimsIdentity(claims, "Test");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "TestScheme");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
